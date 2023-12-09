50509b01ba7e18d3513847b4d18cadb7861650ef
Fatih Fawwaz <fatihfwz87@gmail.com>
2023-12-09 10:38:37 +0700

    perbaikan navbar

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// Mendapatkan elemen navbar
+var navbar = document.querySelector('.navbar');
+var navbarnav = document.querySelector('.navbar-nav');
+
+// Mendapatkan posisi awal navbar
+var sticky = navbar.offsetTop;
+
+// Membuat fungsi untuk mengganti kelas navbar saat di scroll
+function scrollFunction() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add('scrolled'); // Menambahkan kelas scrolled
+    // navbarnav.classList.add('scrolled'); // Menambahkan kelas scrolled
+  } else {
+    navbar.classList.remove('scrolled'); // Menghapus kelas scrolled
+    // navbarnav.classList.remove('scrolled'); // Menghapus kelas scrolled
+  }
+}
+
+// Menambahkan event listener untuk menjalankan fungsi saat di scroll
+window.onscroll = function () {
+  scrollFunction();
+};
+
 $(document).ready(function () {
   const owl = $('.owl-carousel');
   owl.owlCarousel({
