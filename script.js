2780cadbdfa0b4381972041217b22435187842a6
januarsyah901 <januarsyahakbar791@gmail.com>
2023-12-13 11:33:23 +0700

    data json

--- script.js
+++ script.js
@@ -1,5 +1,69 @@
-import content from './content.js';
+//function test
+function log(params) {
+  console.log(params);
+}
+// Fetch data from data.json file
+fetch('data/data.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    // _____Content________________________________
+    data = data["data"]
+    // log(data[1].gambar);
+    // log(data.length);
+    // Membuat sebuah loop yang berjalan sebanyak tiga kali
+    for (var i = 0; i < data.length; i++) {
+      // Membuat elemen <div> baru
+      let contentModal;
+    
+      contentModal = document.createElement('div'); // is a node
+      contentModal.innerHTML = `
+      <div id="myModal${i}" class="modal p-0">
+        <!-- Modal content -->
+        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-fullscreen" style="height: 0; margin: 55px auto; position: absolute">
+          <div class="modal-content" style="position: absolute">
+            <div style="border: none" class="modal-header d-flex justify-content-center">
+              <span id="close${i}" class="d-flex align-items-center">
+                <h2>CLOSE</h2>
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="11" viewBox="0 0 25 11" fill="none">
+                  <path d="M1 1.44678L12.5635 9.06002L24 1.44678" stroke="black" stroke-width="2" stroke-linecap="round" />
+                </svg>
+              </span>
+            </div>
+            <div class="modal-body modal-isi" style="overflow-x: hidden">
+              <div class="px-5">
+                <img src="images/${data[i].gambar}" alt="" class="my-3" />
+    
+                <h1>${data[i].judul}</h1>
+                <p>
+               ${data[i].isi}
+                </p>
+              </div>
+            </div>
+            <div class="modal-footer" style="border: none"></div>
+          </div>
+        </div>
+      `;
+      // console.log(contentModal);
+      // document.getElementById("modalContent").innerHTML(`<h1>hallofirant</h1>`);
+      // contentModal.innerHTML = 'test satu dua tiga';
+      // document.getElementById("modalContent").appendChild(contentModal)
+      document.body.append(contentModal);
+    
+    }
 
+  })
+  .catch(error => {
+    console.error('There has been a problem with your fetch operation:', error);
+  });
+
+
+
+// ________Content end __________________________
 // Get the modal
 const btn = [];
 const modal = [];
@@ -33,55 +97,8 @@ btn.forEach((tombol, i) => {
   };
 });
 
-// _____Content________________________________
+console.log("h");
 
-const contents = new content();
-// Membuat sebuah loop yang berjalan sebanyak tiga kali
-for (var i = 0; i < contents.length; i++) {
-  // Membuat elemen <div> baru
-  var modaltes = document.createElement('div');
-
-  // Memberikan konten HTML pada elemen <div>
-  modaltes.innerHTML = `
-  <div id="myModal${i}" class="modal p-0">
-    <!-- Modal content -->
-    <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-fullscreen" style="height: 0; margin: 55px auto; position: absolute">
-      <div class="modal-content" style="position: absolute">
-        <div style="border: none" class="modal-header d-flex justify-content-center">
-          <span id="close${i}" class="d-flex align-items-center">
-            <h2>CLOSE</h2>
-            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="11" viewBox="0 0 25 11" fill="none">
-              <path d="M1 1.44678L12.5635 9.06002L24 1.44678" stroke="black" stroke-width="2" stroke-linecap="round" />
-            </svg>
-          </span>
-        </div>
-        <div class="modal-body modal-isi" style="overflow-x: hidden">
-          <div class="px-5">
-            <img src="images/${contents[i].gambar}" alt="" class="my-3" />
-
-            <h1>${contents[i].judul}</h1>
-            <p>
-           ${contents[i].isi}
-            </p>
-          </div>
-        </div>
-        <div class="modal-footer" style="border: none"></div>
-      </div>
-    </div>
-  `;
-
-
-  // Menambahkan atribut id pada elemen <div> dengan nomor urut
-  modaltes.id = 'myModal' + i;
-
-  // Menambahkan atribut class pada elemen <div>
-  modaltes.classList.add('modal', 'p-0');
-
-  // Menambahkan elemen <div> ke dalam elemen <body>
-  document.body.appendChild(modaltes);
-
-}
-// ________Content end __________________________
 
 // Mendapatkan elemen navbar
 var navbar = document.querySelector('.navbar');
